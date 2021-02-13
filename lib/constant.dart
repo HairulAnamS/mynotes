1e403e182313434dcc58ba805054486b5777b15e
HairulAnamS <hairulanams23@gmail.com>
2021-02-13 22:20:17 +0700

    finance 6

--- lib/constant.dart
+++ lib/constant.dart
@@ -6,9 +6,10 @@ const int modeNew = 1;
 const int modeEdit = 2;
 const int modeDelete = 3;
 const Color warna = const Color.fromARGB(255, 0, 0, 80);
-final formatter = new NumberFormat.currency(locale: 'id', decimalDigits: 0, symbol: 'Rp.');
+final formatter =
+    new NumberFormat.currency(locale: 'id', decimalDigits: 0, symbol: 'Rp.');
 
-String getBulan(DateTime aDate){
+String getBulan(DateTime aDate) {
   List bulanList = [
     "Januari",
     "Februari",
@@ -26,7 +27,50 @@ String getBulan(DateTime aDate){
   String result;
   int idxBulan = aDate.month;
 
-  result = bulanList[idxBulan-1] + ' ' + DateFormat('yyyy').format(aDate);
+  result = bulanList[idxBulan - 1] + ' ' + DateFormat('yyyy').format(aDate);
   print('bulan lahir : ' + result);
+  return result;
+}
+
+DateTime convertBulan(String aBulan) {
+  DateTime result = new DateTime.now();
+  int month = 1;
+
+  String thn = aBulan.substring(aBulan.length - 4, aBulan.length);
+  String bln = aBulan.substring(0, aBulan.length - 5);
+
+  if (bln == 'Januari')
+    month = 1;
+  else if (bln == 'Februari')
+    month = 2;
+  else if (bln == 'Maret')
+    month = 3;
+  else if (bln == 'April')
+    month = 4;
+  else if (bln == 'Mei')
+    month = 5;
+  else if (bln == 'Juni')
+    month = 6;
+  else if (bln == 'Juli')
+    month = 7;
+  else if (bln == 'Agustus')
+    month = 8;
+  else if (bln == 'September')
+    month = 9;
+  else if (bln == 'Oktober')
+    month = 10;
+  else if (bln == 'November')
+    month = 11;
+  else if (bln == 'Desember') month = 12;
+
+  result = DateTime(int.parse(thn), month, 1);
+  print('tahun:' +
+      thn +
+      ' bulan:' +
+      bln +
+      ' month:' +
+      month.toString() +
+      ' result:' +
+      result.toString());
   return result;
 }
--- lib/grafik_page.dart
+++ lib/grafik_page.dart
@@ -1,3 +1,4 @@
+import 'package:date_format/date_format.dart';
 import 'package:flutter/material.dart';
 import 'package:charts_flutter/flutter.dart' as charts;
 import 'package:google_fonts/google_fonts.dart';
@@ -17,30 +18,29 @@ class _GrafikPageState extends State<GrafikPage> {
   List<Finance> financeList = [];
   DateTime tglSkrg, tglAwal, tglAkhir;
   List<int> income = [];
-  List<int>  outcome = [];
+  List<int> outcome = [];
   String bln0, bln1, bln2;
+  List<String> bulanList = [];
+  String bulan = '';
 
   List<charts.Series> seriesList;
   List<charts.Series<Graph, String>> _createData() {
-    //final random = Random();
     var incomeData = [
       // Graph(bln0, 110),
       // Graph(bln1, 250),
       // Graph(bln2, 300),
-
       Graph(bln0, income[0]),
       Graph(bln1, income[1]),
       Graph(bln2, income[2]),
     ];
 
     var outcomeData = [
-      Graph(bln0, outcome[0]),
-      Graph(bln1, outcome[1]),
-      Graph(bln2, outcome[2]),
-
       // Graph(bln0, 57),
       // Graph(bln1, 99),
       // Graph(bln2, 120),
+      Graph(bln0, outcome[0]),
+      Graph(bln1, outcome[1]),
+      Graph(bln2, outcome[2]),
     ];
 
     return [
@@ -75,11 +75,21 @@ class _GrafikPageState extends State<GrafikPage> {
   @override
   void initState() {
     super.initState();
-    
+
     isLoading = true;
     finance = new Finance();
     financeDB = new FinanceDB();
-    tglSkrg = DateTime.now();
+    generateBulan(DateTime.now());
+    ambilData(false, DateTime.now());
+  }
+
+  void ambilData(bool isFilter, DateTime date) async {
+    isLoading = true;
+    if (isFilter) {
+      tglSkrg = date;
+    } else {
+      tglSkrg = DateTime.now();
+    }
     tglAwal = DateTime(tglSkrg.year, tglSkrg.month - 2, 1);
     tglAkhir = DateTime(tglSkrg.year, tglSkrg.month + 1, 0);
 
@@ -87,10 +97,6 @@ class _GrafikPageState extends State<GrafikPage> {
     bln1 = getBulan(DateTime(tglSkrg.year, tglSkrg.month - 1, 1));
     bln0 = getBulan(DateTime(tglSkrg.year, tglSkrg.month - 2, 1));
 
-    ambilData();
-  }
-
-  void ambilData() async {
     print('start ambil data');
     financeList = await financeDB.getFinanceFilter(tglAwal, tglAkhir);
     getCome(financeList);
@@ -112,15 +118,13 @@ class _GrafikPageState extends State<GrafikPage> {
         } else {
           outcome[0] = outcome[0] + finances[i].nominal;
         }
-      } 
-      else if (finances[i].bulanTrans == bln1) {
+      } else if (finances[i].bulanTrans == bln1) {
         if (finances[i].isDebet) {
           income[1] = income[1] + finances[i].nominal;
         } else {
           outcome[1] = outcome[1] + finances[i].nominal;
         }
-      } 
-      else if (finances[i].bulanTrans == bln2){
+      } else if (finances[i].bulanTrans == bln2) {
         if (finances[i].isDebet) {
           income[2] = income[2] + finances[i].nominal;
         } else {
@@ -130,6 +134,18 @@ class _GrafikPageState extends State<GrafikPage> {
     }
 
     print(' income: $income, outcome: $outcome');
+  }
+
+  void generateBulan(DateTime aDate) {
+    String month; 
+    bulanList.clear();
+
+    for (int i = 0; i < 6; i++) {
+      month = getBulan(aDate);
+      bulanList.add(month);
+      aDate = DateTime(aDate.year, aDate.month - 1, 1);
+    }
+    bulan = bulanList[0];
   }
 
   @override
@@ -142,14 +158,126 @@ class _GrafikPageState extends State<GrafikPage> {
                 textStyle: TextStyle(fontSize: 20, color: Colors.white))),
         backgroundColor: warna,
       ),
-      body: Padding(
-        padding: EdgeInsets.all(10),
-        child: Container(
-          height: MediaQuery.of(context).size.height * 0.73,
-          child: (isLoading)
-              ? Center(child: CircularProgressIndicator())
-              : barChart(),
-        ),
+      body: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.all(8),
+            child: Container(
+              color: warna,
+              height: 50,
+              child: Row(
+                children: [
+                  Flexible(
+                    flex: 1,
+                    child: Container(
+                      margin: EdgeInsets.all(3),
+                      color: Colors.white,
+                      //width: 100,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Row(
+                            children: [
+                              Container(
+                                margin: EdgeInsets.fromLTRB(5, 0, 10, 0),
+                                width: 25,
+                                height: 10,
+                                color: Colors.green,
+                              ),
+                              SizedBox(
+                                width: 3,
+                              ),
+                              Text('Income',
+                                  style: GoogleFonts.play(
+                                      textStyle: TextStyle(
+                                          fontSize: 14, color: warna)))
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Container(
+                                margin: EdgeInsets.fromLTRB(5, 0, 10, 0),
+                                width: 25,
+                                height: 10,
+                                color: Colors.red,
+                              ),
+                              SizedBox(
+                                width: 3,
+                              ),
+                              Text('Outcome',
+                                  style: GoogleFonts.play(
+                                      textStyle: TextStyle(
+                                          fontSize: 14, color: warna)))
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  Flexible(
+                    flex: 2,
+                    child: Container(
+                      margin: EdgeInsets.fromLTRB(0, 3, 3, 3),
+                      color: Colors.white,
+                      child: Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text('Bulan :',
+                                style: GoogleFonts.play(
+                                    textStyle:
+                                        TextStyle(fontSize: 14, color: warna))),
+                          ),
+                          SizedBox(
+                            width: 8,
+                          ),
+                          Container(
+                            width: 150,
+                            //color: Colors.redAccent,
+                            child: DropdownButton(
+                              isExpanded: true,
+                              icon: Icon(Icons.keyboard_arrow_down),
+                              dropdownColor: warna,
+                              hint: Text("Pilih Bulan",
+                                  style: TextStyle(color: Colors.white)),
+                              value: bulan,
+                              items: bulanList.map((value) {
+                                return DropdownMenuItem(
+                                  child: Text(
+                                    value,
+                                    style: GoogleFonts.play(
+                                        textStyle: TextStyle(
+                                            fontSize: 18, color: Colors.green)),
+                                  ),
+                                  value: value,
+                                );
+                              }).toList(),
+                              onChanged: (value) {
+                                setState(() {
+                                  bulan = value;
+                                  ambilData(true, convertBulan(bulan));
+                                });
+                              },
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(10),
+            child: Container(
+              height: MediaQuery.of(context).size.height * 0.63,
+              child: (isLoading)
+                  ? Center(child: CircularProgressIndicator())
+                  : barChart(),
+            ),
+          ),
+        ],
       ),
     );
   }
